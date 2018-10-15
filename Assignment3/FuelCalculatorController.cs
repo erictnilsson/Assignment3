1f994aec52a6e20cc3fd7bd1eb96bc9359fa67c5
Eric Nilsson <ericnilssont@gmail.com>
2018-10-15 11:40:43 +0200

    Initial commit

---
+++ Assignment3/FuelCalculatorController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{ // error handle pls
+    class FuelCalculatorController
+    {
+        private FuelCalculator fc; 
+
+        public FuelCalculatorController(double odoCurr, double odoPrev, double fuel, double literPrice)
+        {
+            fc = new FuelCalculator(odoCurr, odoPrev, fuel, literPrice); 
+        }
+
+        public double GetLiterPerKm()
+        {
+            return fc.GetLiterPerKm(); 
+        }
+
+        public double GetKmPerLiter()
+        {
+            return fc.GetKmPerLiter(); 
+        }
+
+        public double GetLiterPerMetricMile()
+        {
+            return fc.GetLiterPerMetricMile(); 
+        }
+
+        public double GetLiterPerSwedishMile()
+        {
+            return fc.GetLiterPerSwedishMile(); 
+        }
+
+        public double GetCostPerKm()
+        {
+            return fc.GetCostPerKm(); 
+        }
+    }
+}
